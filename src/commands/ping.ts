a60fa8ea2f4897360164dfcefbb11d7074e9fb32
Rdeisenroth <rdeisenroth@gmail.com>
2021-07-11 00:11:28 +0200

    Ping and Help Command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -11,19 +11,17 @@ const command: Command = {
     cooldown: 0,
     guildOnly: true,
     execute: async (client, message, args) => {
-        var m = await message.channel.send("Pinging...");
-        let ping = m.createdTimestamp - message.createdTimestamp
+        var m = await message!.channel.send("Pinging...");
+        let ping = m.createdTimestamp - message!.createdTimestamp
         let choices = ["Is this really my ping, it's so high...", "Is it okay? I cant look", "I hope it isnt bad"]
         let response = choices[Math.floor(Math.random() * choices.length)];
         let embed = new MessageEmbed()
-            //.setAuthor(client.user.username, client.user.displayAvatarURL)
             .setTitle('__Response Times__')
-            .setColor(message.guild?.me?.roles.highest.color || 0x7289da)
+            .setColor(message!.guild?.me?.roles.highest.color || 0x7289da)
             .addField('Bot Latency:', ':hourglass_flowing_sand:' + ping + 'ms', true)
             .addField('API Latency:', ':hourglass_flowing_sand:' + Math.round(client.ws.ping) + 'ms', true)
-        // .setThumbnail(client.user!.avatarURL()!);
         await m.edit('Pong.');
-        message.channel.send(embed);
+        message!.channel.send(embed);
     }
 }
 
