938c20f9f4141cd3491cfb3329ac1426c04f36ed
Rdeisenroth <rdeisenroth@gmail.com>
2021-10-17 14:39:59 +0200

    Fix Setqueue Command

--- src/commands/setqueue.ts
+++ src/commands/setqueue.ts
@@ -2,7 +2,6 @@ import { GuildChannel, Message } from "discord.js";
 import { Command } from "../../typings";
 import GuildSchema from "../models/guilds";
 import { VoiceChannel } from "../models/voice_channels";
-import mongoose from "mongoose";
 
 const command: Command = {
     name: "setqueue",
@@ -38,40 +37,43 @@ const command: Command = {
             return;
         }
         if (interaction instanceof Message) {
-            client.utils.embeds.SimpleEmbed(interaction, "Slash Only Command", "This Command is Slash only but you Called it with The Prefix. use the slash Command instead.");
-            return;
+            return await client.utils.embeds.SimpleEmbed(interaction, "Slash Only Command", "This Command is Slash only but you Called it with The Prefix. use the slash Command instead.");
         }
-        const owner = client.users.cache.find(m => m.id == client.ownerID);
-        const member = client.utils.general.getMember(interaction);
-        if (!member || member.id !== client.ownerID as string) {
-            await interaction?.reply("You do not have permission to execute this command.");
-            return;
-        }
-
-        const g = interaction!.guild!;
+        await interaction.deferReply();
+        const g = interaction.guild!;
 
         // Find channel
         // const channel = g!.channels.cache.find(x => x.id == args[0]);
         const channel = interaction.options.getChannel("channel", true);
         if (!channel) {
-            return await interaction.reply("Channel could not be found.");
+            return await client.utils.embeds.SimpleEmbed(interaction, "Voice Channel System", ":x: Channel could not be found.");
         }
         if (!(channel instanceof GuildChannel)) {
-            return await interaction.reply("Channel is invalid.");
+            return await client.utils.embeds.SimpleEmbed(interaction, "Voice Channel System", ":x: Channel is invalid.");
         }
         const guildData = (await GuildSchema.findById(g.id))!;
-        const waitingroom_channel: VoiceChannel & {_id:string} = {
+        const queueName = interaction.options.getString("queue", true);
+        const queueData = guildData.queues.find(x => x.name.toLowerCase() === queueName.toLowerCase());
+        if (!queueData) {
+            return await client.utils.embeds.SimpleEmbed(interaction, "Voice Channel System", ":x: Queue could not be found.");
+        }
+        if (guildData.voice_channels.some(x => x.queue === queueData._id)) {
+            return await client.utils.embeds.SimpleEmbed(interaction, "Voice Channel System", ":x: Queue already linked to another Channel.");
+        }
+
+        const supervisor_role = interaction.options.getRole("supervisor", true);
+        const waitingroom_channel: VoiceChannel & { _id: string } = {
             _id: channel.id,
             channel_type: 2,
             locked: false,
             managed: true,
             permitted: [],
-            queue: new mongoose.Types.ObjectId(interaction.options.getString("queue", true)),
-            supervisors: interaction.options.getRole("supervisor") ? [interaction.options.getRole("supervisor")!.id] : undefined,
+            queue: queueData._id,
+            supervisors: [supervisor_role.id],
         };
         guildData.voice_channels.push(waitingroom_channel);
         await guildData.save();
-        await client.utils.embeds.SimpleEmbed(interaction, "Queue System", "The Queue was Linked to this Channel.");
+        await client.utils.embeds.SimpleEmbed(interaction, "Queue System", `:white_check_mark: The Queue was Linked to the Channel ${channel}.`);
     },
 };
 
