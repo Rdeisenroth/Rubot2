152b582a47df7989d0c5503618f7e8db2d7cd05a
Ruben Deisenroth <rdeisenroth@gmail.com>
2021-10-25 02:57:57 +0200

    Add Tutor queue List command to list entries

--- src/commands/coach/queue/list.ts
+++ src/commands/coach/queue/list.ts
@@ -1,6 +1,8 @@
-import { Message } from "discord.js";
+import { EmbedFieldData, Message } from "discord.js";
 import path from "path";
 import { Command } from "../../../../typings";
+import GuildSchema from "../../../models/guilds";
+import UserSchema from "../../../models/users";
 
 const command: Command = {
     name: "list",
@@ -22,8 +24,61 @@ const command: Command = {
         }
 
         const g = interaction.guild!;
+        const guildData = (await GuildSchema.findById(g.id));
+        if (!guildData) {
+            return await client.utils.embeds.SimpleEmbed(interaction, { title: "Coaching System", text: "Guild Data Could not be found.", empheral: true });
+        }
 
-        client.utils.embeds.SimpleEmbed(interaction, "TODO", `Command \`${path.relative(process.cwd(), __filename)}\` is not Implemented Yet.`);
+        const user = client.utils.general.getUser(interaction);
+        const userEntry = await UserSchema.findOneAndUpdate({ _id: user.id }, { _id: user.id }, { new: true, upsert: true, setDefaultsOnInsert: true });
+        // Check if User has Active Sessions
+        const activeSessions = await userEntry.getActiveSessions();
+        // We expect at most 1 active session per guild
+        const coachingSession = activeSessions.find(x => x.guild && x.guild === g.id);
+        if (!coachingSession) {
+            return await client.utils.embeds.SimpleEmbed(interaction, { title: "Coaching System", text: "You Have no Active Coaching Session.", empheral: true });
+        }
+        if (!coachingSession.queue) {
+            return await client.utils.embeds.SimpleEmbed(interaction, { title: "Coaching System", text: "There is no Queue Linked to your Session.", empheral: true });
+        }
+
+        const queue = coachingSession.queue;
+        const queueData = guildData.queues.id(queue);
+        if (!queueData) {
+            return await client.utils.embeds.SimpleEmbed(interaction, { title: "Coaching System", text: "Queue Could not be Found.", empheral: true });
+        }
+
+        // await g.members.fetch();
+
+        const fields: EmbedFieldData[] = [];
+        for (const e of queueData.getSortedEntries(10)) {
+            const position = queueData.getPosition(e.discord_id)+1;
+            const joined_at = `<t:${Math.round((+e.joinedAt) / 1000)}:f>`;
+            const intent = e.intent;
+            const member = await g.members.fetch(e.discord_id);
+            fields.push({
+                name: member.displayName, value:
+                    `-Position: ${position}`
+                        + `\n-joined at: ${joined_at}`
+                        + (intent ? `\n-intent: ${intent}` : ""),
+            });
+        }
+        // let fields = await queueData.getSortedEntries(10).map(async x => {
+        //     return {
+        //         title: await g.members.fetch(x.discord_id),
+        //         text: `- Position: ${queueData.getPosition(x.discord_id)}`
+        //             + `\n-joined at: <t:${(+x.joinedAt) / 1000}:f>`
+        //             + `${x.intent ? `\n-intent: ${x.intent}` : ""}`,
+        //     }
+        // });
+
+        await client.utils.embeds.SimpleEmbed(interaction, {
+            title: "Queue Information",
+            text: fields&&fields.length ? `Queue Entries of ${queueData.name}` : `Queue ${queueData.name} is empty`,
+            empheral: true,
+            fields,
+        });
+        // client.utils.embeds.SimpleEmbed(interaction, "TODO", `Command \`${path.relative(process.cwd(), __filename)}\` is not Implemented Yet.`);
     },
 };
 
