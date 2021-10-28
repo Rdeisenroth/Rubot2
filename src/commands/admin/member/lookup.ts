1372f6e840057654308f5a39e716564eb66949ac
Ruben Deisenroth <rdeisenroth@gmail.com>
2021-10-29 01:05:05 +0200

    Add member lookup command

---
+++ src/commands/admin/member/lookup.ts
@@ -0,0 +1,59 @@
+import { EmbedFieldData, Message } from "discord.js";
+import moment from "moment";
+import { Command } from "../../../../typings";
+import GuildSchema from "../../../models/guilds";
+import UserSchema from "../../../models/users";
+import SessionSchema from "../../../models/sessions";
+import QueueSchema from "../../../models/queues";
+
+const command: Command = {
+    name: "lookup",
+    description: "gets user infos",
+    aliases: ["l"],
+    usage: "[channel resolvable]",
+    cooldown: 3000,
+    category: "Miscellaneous",
+    options: [
+        {
+            name: "user",
+            description: "The User to check",
+            type: "USER",
+            required: true,
+        },
+    ],
+    guildOnly: true,
+    execute: async (client, interaction, args) => {
+        if (!interaction) {
+            return;
+        }
+        if (interaction instanceof Message) {
+            client.utils.embeds.SimpleEmbed(interaction, "Slash Only Command", "This Command is Slash only but you Called it with The Prefix. use the slash Command instead.");
+            return;
+        }
+        await interaction.deferReply({ephemeral:true});
+        let user = interaction.options.getUser("user", true);
+        user = await user.fetch();
+        let userData = await UserSchema.findById(user.id);
+
+        if (!userData) {
+            return await client.utils.embeds.SimpleEmbed(interaction, { title: "Verification System", text: `User ${user} not found in database.`, empheral: true });
+        }
+
+        const fields: EmbedFieldData[] = [
+            { name: "> TU-ID", value: `${userData.tu_id}` },
+            { name: "> Moodle-ID", value: `${userData.moodle_id}` },
+        ];
+
+        await client.utils.embeds.SimpleEmbed(interaction, {
+            title: "Verification System",
+            text: `Information about User ${user}`,
+            empheral: true,
+            fields,
+        });
+    },
+};
+
+/**
+ * Exporting the Command using CommonJS
+ */
+module.exports = command;
