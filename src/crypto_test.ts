9cae223616eec5d5aa6c40d3255aab5102cc5158
Rdeisenroth <rdeisenroth@gmail.com>
2022-04-13 16:21:04 +0200

    token skript

--- src/crypto_test.ts
+++ src/crypto_test.ts
@@ -1,23 +1,80 @@
-import * as crypto from "crypto";
-import {verify_secret} from "../config.json";
+import * as cryptojs from "crypto-js";
+import { verify_secret } from "../config.json";
+import { InternalRoles } from "./models/bot_roles";
+import { parse } from "csv-parse";
+import * as fs from "fs";
+import * as path from "path";
 
-// encrypt text with aes-256-cbc
-export function encryptText(text: string): string {
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(verify_secret), iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return iv.toString("hex") + ":" + encrypted.toString("hex");
+/**
+ * Encrypts a given Text
+ * @param text The Text to encrypt
+ * @returns the encrypted Text
+ */
+export function encryptText(text: string) {
+    return cryptojs.AES.encrypt(text, verify_secret).toString();
 }
 
-export function decryptText(text: string): string {
-    const textParts = text.split(":");
-    const iv = Buffer.from(textParts.shift()!, "hex");
-    const encryptedText = Buffer.from(textParts.join(":"), "hex");
-    const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(verify_secret), iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
+/**
+ * Decrypts a given Text
+ * @param text the Text to decrypt
+ * @returns the decrypted Text
+ */
+export function decryptText(text: string) {
+    return cryptojs.AES.decrypt(text, verify_secret).toString(cryptojs.enc.Utf8);
 }
 
-console.log(encryptText("Das ist ein Test"));
+/**
+ * Generates an encrypted Token-String with the given parameters
+ * @param server_id The ID of the Server
+ * @param version_id The Token Version
+ * @param tu_id The TU ID
+ * @param moodle_id The Moodle ID
+ * @param internal_role_names The internal role names
+ * @returns The generated Token
+ */
+export function encryptTokenString(server_id: string, version_id: string, tu_id: string, moodle_id: string, internal_role_names: InternalRoles[]): string {
+    const token = `${server_id}|${version_id}|${tu_id}|${moodle_id}|${internal_role_names.join(",")}`;
+    const crypted_token_string = encryptText(token);
+    return crypted_token_string;
+}
+
+type Student = {
+    id_tu: string;
+    id_moodle: string;
+    first_access: Date;
+};
+
+(() => {
+    const csvFilePath = path.resolve(__dirname, "report.csv");
+
+    const headers = ["id_tu", "id_moodle", "first_access"];
+
+    const fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" });
+
+    parse(fileContent, {
+        delimiter: ",",
+        columns: headers,
+    }, (error, result: Student[]) => {
+        if (error) {
+            console.error(error);
+        }
+        // {"moodleId":"1","token":"FOP-DiscordV1|guest|1#5630c38fb1ae39b1048e0cf802f4170dba8943f5e13510055357b3cb67a3bac1"}
+        const newResult = result.map((student) => {
+            const token = encryptTokenString(
+                "940632262272237568",
+                "01",
+                student.id_tu,
+                student.id_moodle,
+                [InternalRoles.VERIFIED],
+            );
+            return {
+                moodleId: student.id_moodle,
+                token,
+            };
+        });
+        console.log("Result", newResult);
+
+        // write result to result.json file
+        fs.writeFileSync(path.resolve(__dirname, "result.json"), JSON.stringify(newResult, null, 4));
+    });
+})();
