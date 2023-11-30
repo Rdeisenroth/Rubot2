b0621baa3236d6902e1b510e2c211a7540781dac
Rdeisenroth <rdeisenroth@gmail.com>
2023-11-30 18:54:47 +0100

    improve genverifyroles

--- src/commands/admin/genverifyroles.ts
+++ src/commands/admin/genverifyroles.ts
@@ -1,9 +1,10 @@
-import { Message, Role } from "discord.js";
+import { InternalGuildRoles } from "../../models/bot_roles";
+import { Message, ApplicationCommandOptionType } from "discord.js";
 import { Command } from "../../../typings";
 import "moment-duration-format";
 import { GuildModel } from "../../models/guilds";
-import { DBRole, InternalRoles, RoleScopes } from "../../models/bot_roles";
-import { ArraySubDocumentType, mongoose } from "@typegoose/typegoose";
+import { RoleScopes } from "../../models/bot_roles";
+import { mongoose } from "@typegoose/typegoose";
 
 
 
@@ -11,9 +12,17 @@ import { ArraySubDocumentType, mongoose } from "@typegoose/typegoose";
  * The Command Definition
  */
 const command: Command = {
-    name: "genverifyroles",
+    name: "updatebotroles",
     guildOnly: false,
-    description: "Temp Command that fixes the Verify DB.",
+    description: "This command updates or generates the database entries for the internal roles",
+    options: [
+        {
+            name: "create-if-not-exists",
+            description: "Create the Roles on the guild with the default name if they don't exist, defaults to true",
+            type: ApplicationCommandOptionType.Boolean,
+            required: false,
+        },
+    ],
     async execute(client, interaction, args) {
         if (!interaction || !interaction.guild) {
             return;
@@ -22,25 +31,53 @@ const command: Command = {
             client.utils.embeds.SimpleEmbed(interaction, "Slash Only Command", "This Command is Slash only but you Called it with The Prefix. use the slash Command instead.");
             return;
         }
+
         await interaction.deferReply();
         const guild = interaction.guild;
-        const roles = await interaction.guild.roles.fetch();
-        const members = await interaction.guild.members.fetch();
-        const dbGuild = (await GuildModel.findOne({ _id: guild.id }))!;
-        const verifiedRole = interaction.guild.roles.cache.find(x => x.name.toLowerCase() === "verified");
-        let dbVerifyRole = dbGuild.guild_settings.roles?.find(x => x.internal_name === InternalRoles.VERIFIED);
-        const orgaRole = interaction.guild.roles.cache.find(x => x.name.toLowerCase() === "orga");
-        let dbOrgaRole = dbGuild.guild_settings.roles?.find(x => x.internal_name === InternalRoles.SERVER_ADMIN);
-        const tutorRole = interaction.guild.roles.cache.find(x => x.name.toLowerCase() === "tutor");
-        let dbTutorRole = dbGuild.guild_settings.roles?.find(x => x.internal_name === InternalRoles.TUTOR);
-        const activeSessionRole = interaction.guild.roles.cache.find(x => x.name.toLowerCase() === "active_session");
-        let dbActiveSessionRole = dbGuild.guild_settings.roles?.find(x => x.internal_name === InternalRoles.ACTIVE_SESSION);
+        const dbGuild = await GuildModel.findById(guild.id);
+        if (!dbGuild) {
+            return await client.utils.embeds.SimpleEmbed(interaction, "Adminstration", "Guild not found in Database.");
+        }
 
+        const createIfNotExists = interaction.options.getBoolean("create-if-not-exists", false) ?? true;
 
 
         // Create the roles if they don't exist
-        for (const [r, dbr, irn] of ([[verifiedRole, dbVerifyRole, InternalRoles.VERIFIED], [orgaRole, dbOrgaRole, InternalRoles.SERVER_ADMIN], [tutorRole, dbTutorRole, InternalRoles.TUTOR], [activeSessionRole, dbActiveSessionRole, InternalRoles.ACTIVE_SESSION]] as [Role, ArraySubDocumentType<DBRole>, InternalRoles][])) {
-            if (!r) continue;
+        // iterate over type InternalGuildRoles
+        for (const irn of InternalGuildRoles) {
+            let r = guild.roles.cache.find(x => x.name === irn);
+            const dbr = dbGuild.guild_settings.roles?.find(x => x.internal_name === irn);
+            if (!r) {
+                // try to find role by id from db
+                if (dbr) {
+                    const newR = interaction.guild.roles.resolve(dbr.role_id!);
+                    if (newR) {
+                        r = newR;
+                        if (dbr.server_role_name !== r.name) {
+                            console.log(`Role "${irn}" was renamed from "${dbr.server_role_name}" to "${r.name}". Updating DB`);
+                            // update db
+                            dbr.server_role_name = r.name;
+                            await dbGuild.save();
+                        }
+                        continue;
+                    }
+                }
+                if (!createIfNotExists) {
+                    console.log(`role ${irn} not found in guild ${guild.name}. Skipping`);
+                    continue;
+                }
+                console.log(`role ${irn} not found in guild ${guild.name}. Creating`);
+                const newRole = await guild.roles.create({
+                    name: irn,
+                    mentionable: false,
+                });
+                if (!newRole) {
+                    console.log(`Could not create role ${irn}`);
+                    continue;
+                }
+                console.log(`created role ${irn}`);
+                r = newRole;
+            }
             if (!dbr) {
                 console.log(`creating role ${irn}`);
                 if (!dbGuild.guild_settings.roles) {
@@ -54,17 +91,16 @@ const command: Command = {
                     server_role_name: r.name,
                 });
                 await dbGuild.save();
-                if(irn === InternalRoles.VERIFIED) {
-                    dbVerifyRole = dbGuild.guild_settings.roles.find(x => x.role_id === r.id)!;
-                } else if(irn === InternalRoles.SERVER_ADMIN) {
-                    dbOrgaRole = dbGuild.guild_settings.roles.find(x => x.role_id === r.id)!;
-                } else if(irn === InternalRoles.TUTOR) {
-                    dbTutorRole = dbGuild.guild_settings.roles.find(x => x.role_id === r.id)!;
-                } else if(irn === InternalRoles.ACTIVE_SESSION) {
-                    dbActiveSessionRole = dbGuild.guild_settings.roles.find(x => x.role_id === r.id)!;
-                }
             }
         }
+        await client.utils.embeds.SimpleEmbed(
+            interaction,
+            "Adminstration",
+            `Done generating internal Roles. Internal Roles: \n
+            ${(dbGuild.guild_settings.roles?.map(x => `❯ ${x.internal_name}: <@&${x.role_id}>`).join("\n") ?? "None")}
+            \nUnassigned Roles: \n
+            ${InternalGuildRoles.filter(x => !dbGuild.guild_settings.roles?.find(y => y.internal_name === x)).map(x => `❯ ${x}`).join("\n") ?? "None"}`,
+        );
     },
 };
 
