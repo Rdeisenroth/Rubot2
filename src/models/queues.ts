2b187efdeb15a00080316238e297ec368dbfe7e0
Rdeisenroth <rdeisenroth@gmail.com>
2023-12-06 22:10:45 +0100

    lint

--- src/models/queues.ts
+++ src/models/queues.ts
@@ -17,92 +17,92 @@ export class Queue {
      * The Name Of The Queue
      */
     @prop({ required: true })
-    name!: string;
+        name!: string;
     /**
      * A Description of the Queue
      */
     @prop()
-    description?: string;
+        description?: string;
     /**
      * The max Amount of Users that the queue can handle
      */
     @prop()
-    limit?: number;
+        limit?: number;
     /**
      * The Timeout in Milliseconds if the User disconnects from the Queue (usefull for VC based Queues)
      */
     @prop()
-    disconnect_timeout?: number;
+        disconnect_timeout?: number;
     /**
      * The Timeout in Milliseconds that the user is kicked off the queue After not accepting a match
      */
     @prop()
-    match_timeout?: number;
+        match_timeout?: number;
     /**
      * A Custom Join Message for the Queue. Use ${pos} ${total} ${eta} ${user} and so on to create Dynamic Messages.
      */
     @prop()
-    join_message?: string;
+        join_message?: string;
     /**
      *  A Custom Match Found Message for the Queue. Use ${pos} ${total} ${eta} ${user} ${match} ${match_channel} and so on to create Dynamic Messages.
      */
     @prop()
-    match_found_message?: string;
+        match_found_message?: string;
     /**
      *  A Custom Timeout Message. Use ${pos} ${total} ${eta} ${user} ${timeout} and so on to create Dynamic Messages.
      */
     @prop()
-    timeout_message?: string;
+        timeout_message?: string;
     /**
      *  A Custom Leave Message. Use ${pos} ${total} ${eta} ${user} ${timeout} and so on to create Dynamic Messages.
      */
     @prop()
-    leave_message?: string;
+        leave_message?: string;
     /**
      * A Custom Message that is Displayed when the Room is Left (like Please confirm ur stay)
      */
     @prop()
-    leave_room_message?: string;
+        leave_room_message?: string;
     /**
      * A Template for spawning in Rooms (if empty default template is used)
      */
     @prop({ type: () => VoiceChannelSpawner })
-    room_spawner?: SubDocumentType<VoiceChannelSpawner>;
+        room_spawner?: SubDocumentType<VoiceChannelSpawner>;
     /**
      * A text Channel to use if dms are disabled
      */
     @prop()
-    text_channel?: string;
+        text_channel?: string;
     /**
      * Whether the queue is locked (this also disables the /queue join command for this queue)
      */
     @prop({ default: false })
-    locked?: boolean;
+        locked?: boolean;
     /**
      * Whether to automatically lock and unlock the queue according to the opening_times
      */
     @prop({ default: false })
-    auto_lock?: boolean;
+        auto_lock?: boolean;
     /**
      * The opening times of the Queue
      */
     @prop({ type: QueueSpan, default: [], required: true })
-    opening_times!: mongoose.Types.DocumentArray<ArraySubDocumentType<QueueSpan>>;
+        opening_times!: mongoose.Types.DocumentArray<ArraySubDocumentType<QueueSpan>>;
     /**
      * The standard time to shift the unlocking of the queue by in milliseconds
      */
     @prop({ default: 0 })
-    openShift?: number;
+        openShift?: number;
     /**
      * The standard time to shift the locking of the queue by in milliseconds
      */
     @prop({ default: 0 })
-    closeShift?: number;
+        closeShift?: number;
     /**
      * The Entries of the Queue
      */
     @prop({ type: QueueEntry, default: [], required: true })
-    entries!: mongoose.Types.DocumentArray<ArraySubDocumentType<QueueEntry>>;
+        entries!: mongoose.Types.DocumentArray<ArraySubDocumentType<QueueEntry>>;
 
     /**
      * Put an Entry into the Queue
