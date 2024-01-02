ec95d82e63bc6f7ad5326c833993e211377db556
niklhut <49069026+niklhut@users.noreply.github.com>
2024-01-02 21:49:14 +0100

    Remove unused error

--- src/types/RoleCreationError.ts
+++
@@ -1,5 +0,0 @@
-export class RoleCreationError extends Error {
-    constructor(roleName: string, guildName: string) {
-        super(`Role Creation Error: Role "${roleName}" could not be created on guild "${guildName}"`);
-    }
-}
