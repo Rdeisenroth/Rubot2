bae653a4228e1924d3ded1d21333f7388a18396f
niklhut <49069026+niklhut@users.noreply.github.com>
2024-01-03 10:24:43 +0100

    Add guild create event tests

---
+++ tests/events/GuildCreateEvent.test.ts
@@ -0,0 +1,52 @@
+import { container } from "tsyringe"
+import GuildCreateEvent from "../../src/events/GuildCreateEvent"
+import { MockDiscord } from "../mockDiscord"
+import { Guild } from "discord.js"
+import { CommandsManager } from "../../src/managers"
+import { GuildModel } from "../../src/models/Guild"
+
+describe("GuildCreateEvent", () => {
+    const event = GuildCreateEvent
+    const discord = container.resolve(MockDiscord)
+    let eventInstance: GuildCreateEvent
+    let guild: Guild
+
+    beforeEach(() => {
+        eventInstance = new event(discord.getClient())
+        guild = discord.mockGuild()
+        guild.commands.set = jest.fn().mockImplementation(() => Promise.resolve())
+    })
+
+    it("should have the correct name", () => {
+        expect(event.name).toBe("guildCreate")
+    })
+
+    it("should log the guild name and id", async () => {
+        const logSpy = jest.spyOn(discord.getClient().logger, 'success')
+        await eventInstance.execute(guild)
+
+        expect(logSpy).toHaveBeenCalledWith(`Joined guild ${guild.name} (id: ${guild.id})`)
+    })
+
+    it("should create a new guild in the database", async () => {
+        const findSpy = jest.spyOn(GuildModel, 'findById')
+        const saveSpy = jest.spyOn(GuildModel.prototype, 'save')
+        await eventInstance.execute(guild)
+
+        expect(findSpy).toHaveBeenCalledTimes(1)
+        expect(findSpy).toHaveBeenCalledWith(guild.id)
+
+        expect(saveSpy).toHaveBeenCalledTimes(1)
+        const saveSpyRes = await saveSpy.mock.results[0].value
+        expect(saveSpyRes).toMatchObject({ _id: guild.id })
+    })
+
+    it("should register slash commands for the guild", async () => {
+        const commandsManager = container.resolve(CommandsManager)
+        const registerSpy = jest.spyOn(commandsManager, 'registerSlashCommandsFor')
+        await eventInstance.execute(guild)
+
+        expect(registerSpy).toHaveBeenCalledTimes(1)
+        expect(registerSpy).toHaveBeenCalledWith(guild)
+    })
+})
