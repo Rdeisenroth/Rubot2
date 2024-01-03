3ce2f7731b9549370eed980c3e7b4fe50d3e7852
niklhut <49069026+niklhut@users.noreply.github.com>
2024-01-03 16:09:02 +0100

    Add support to register subcommands

--- src/managers/CommandsManager.ts
+++ src/managers/CommandsManager.ts
@@ -1,6 +1,7 @@
-import { ApplicationCommandOptionData, ChatInputApplicationCommandData, Guild } from "discord.js";
+import { ApplicationCommandOptionData, ApplicationCommandOptionType, BaseChannel, ChatInputApplicationCommandData, Guild } from "discord.js";
 import { Bot } from "../Bot";
 import { delay, inject, injectable, singleton } from "tsyringe";
+import { BaseCommand, BaseCommandOrSubcommandHandler, BaseSubcommandHandler } from "../baseCommand";
 
 @injectable()
 @singleton()
@@ -10,7 +11,7 @@ export default class CommandsManager {
 
     constructor(@inject(delay(() => Bot)) client: Bot) {
         this.client = client;
-        this.loadCommandsData();
+        this.commandsData = this.loadCommandsData(this.client.commands);
     }
 
     public async registerSlashCommandsFor(guild: Guild): Promise<void> {
@@ -23,14 +24,53 @@ export default class CommandsManager {
         }
     }
 
-    private loadCommandsData() {
-        for (const command of this.client.commands) {
-            const commandData: ChatInputApplicationCommandData = {
-                name: command.name,
-                description: command.description,
-                options: command.options as ApplicationCommandOptionData[],
-            };
-            this.commandsData.push(commandData);
+    private loadCommandsData(commands: typeof BaseCommandOrSubcommandHandler[]): ChatInputApplicationCommandData[] {
+        const commandsData: ChatInputApplicationCommandData[] = [];
+
+        for (const command of commands) {
+            const commandData = this.loadCommandData(command);
+            commandsData.push(commandData);
         }
+
+        return commandsData;
+    }
+
+    private loadCommandData(command: typeof BaseCommandOrSubcommandHandler): ChatInputApplicationCommandData {
+        if (command.prototype instanceof BaseCommand) {
+            return this.loadBaseCommandData(command as typeof BaseCommand);
+        } else if (command.prototype instanceof BaseSubcommandHandler) {
+            return this.loadBaseSubcommandHandlerData(command as typeof BaseSubcommandHandler);
+        }
+        throw new Error(`Command ${command.name} is neither a BaseCommand nor a BaseSubcommandHandler.`);
+    }
+
+    private loadBaseCommandData(command: typeof BaseCommand): ChatInputApplicationCommandData {
+        const commandData: ChatInputApplicationCommandData = {
+            name: command.name,
+            description: command.description,
+            options: command.options as ApplicationCommandOptionData[],
+        };
+        return commandData;
+    }
+
+    private loadBaseSubcommandHandlerData(command: typeof BaseSubcommandHandler): ChatInputApplicationCommandData {
+        const baseSubcommandHandler = command as typeof BaseSubcommandHandler;
+        const subcommandsData = this.loadCommandsData(baseSubcommandHandler.subcommands);
+        const subcommandOptions = subcommandsData.map(subcommandData => {
+            const subcommandDataOptions = subcommandData.options!;
+            const subcommandIsSubcommandHandler = subcommandDataOptions.flatMap(option => option.type).includes(ApplicationCommandOptionType.Subcommand);
+            return {
+                name: subcommandData.name,
+                description: subcommandData.description,
+                type: subcommandIsSubcommandHandler ? ApplicationCommandOptionType.SubcommandGroup : ApplicationCommandOptionType.Subcommand,
+                options: subcommandData.options,
+            }
+        })
+        const commandData: ChatInputApplicationCommandData = {
+            name: command.name,
+            description: command.description,
+            options: subcommandOptions as ApplicationCommandOptionData[],
+        }
+        return commandData;
     }
 }
