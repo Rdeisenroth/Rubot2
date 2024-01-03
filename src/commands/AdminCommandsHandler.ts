f34b610cc6b83ae57b026794022b13a2d9ed7434
niklhut <49069026+niklhut@users.noreply.github.com>
2024-01-03 22:11:31 +0100

    Add admin queue command handler

--- src/commands/AdminCommandsHandler.ts
+++ src/commands/AdminCommandsHandler.ts
@@ -1,14 +1,13 @@
 import { BaseSubcommandsHandler } from "../baseCommand";
-import QueueCommandsHandler from "./admin/QueueCommandsHandler";
+import AdminQueueCommandsHandler from "./admin/AdminQueueCommandsHandler";
 import UpdateBotRolesCommand from "./admin/UpdateBotRoles";
-import { Bot } from "../Bot";
 
 export default class AdminCommandsHandler extends BaseSubcommandsHandler {
     public static name = "admin";
     public static description = "Admin command handler.";
 
     public static subcommands = [
-        QueueCommandsHandler,
+        AdminQueueCommandsHandler,
         UpdateBotRolesCommand
     ]
 }
