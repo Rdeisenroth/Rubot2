f18adedbde80e1402f1c38b5896d80bf00e66858
niklhut <49069026+niklhut@users.noreply.github.com>
2024-01-04 23:06:43 +0100

    Add documentation

--- src/types/errors/QueueAlreadyExistsError.ts
+++ src/types/errors/QueueAlreadyExistsError.ts
@@ -1,6 +1,13 @@
 export default class QueueAlreadyExistsError extends Error {
+    /**
+     * The queue name which already exists.
+     */
     public queueName: string;
 
+    /**
+     * Creates a new queue already exists error.
+     * @param queueName The queue name which already exists.
+     */
     constructor(queueName: string) {
         super(`Queue ${queueName} already exists.`);
         this.queueName = queueName;
