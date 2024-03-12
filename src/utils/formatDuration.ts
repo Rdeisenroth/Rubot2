9182356e446507f8fb927e7205dce9fa5e878403
niklhut <49069026+niklhut@users.noreply.github.com>
2024-03-12 18:17:52 +0100

    Format duration with milliseconds

--- src/utils/formatDuration.ts
+++ src/utils/formatDuration.ts
@@ -1,4 +1,12 @@
+/**
+ * Formats a duration in milliseconds into a string representation.
+ * The format is "Xh Ym Zs", where X represents hours, Y represents minutes, and Z represents seconds.
+ * 
+ * @param duration - The duration in milliseconds to format.
+ * @returns A string representation of the formatted duration.
+ */
 export function formatDuration(duration: number): string {
+    duration = duration / 1000;
     const hours = Math.floor(duration / 3600);
     const minutes = Math.floor((duration % 3600) / 60);
     const seconds = duration % 60;
